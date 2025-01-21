2cafa0ecae4add876547e7826ed79aacf9d747c2
Ankur Hambir <hambir.ankur@gmail.com>
2025-01-21 01:17:06 -0600

    Express code

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
-const express = require('express')
+import express from 'express';
+import mongoose from 'mongoose';
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+main().then(()=>{console.log("connected")}).catch(err => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb+srv://hambirankur:Ashu%402410@merndb.nidf2.mongodb.net/?retryWrites=true&w=majority&appName=merndb');
+
+  app.use("/",(req,res)=>{
+    res.send("Book serve is running");
+  })
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
